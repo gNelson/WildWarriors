8897577967062b34c962b0816bc4a1859870e59f
gNelson <nelson.gary.l@gmail.com>
2015-06-08 15:50:00 -0500

    Initial Checkin

---
+++ Source/WildWarriors/WildWarriorsGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "WildWarriorsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AWildWarriorsGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	AWildWarriorsGameMode(const FObjectInitializer& ObjectInitializer);
+};
+
+
+
